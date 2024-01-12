5122c08950580482a76e7b347756ac874ab2542d
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-01-13 03:33:05 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] d = new int[1005];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        d[1] = 1;
+        d[2] = 3;
+        for(int i = 3; i <= n; i++) {
+            d[i] = (d[i - 1] + 2 * d[i - 2]) % 10007;
+        }
+        System.out.println(d[n]);
+    }
+}
