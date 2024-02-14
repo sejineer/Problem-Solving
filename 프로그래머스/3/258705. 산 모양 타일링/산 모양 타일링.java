ecb4b9d6db9fd033cb5f9a4feeaab2abbe4e8b12
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-02-14 16:25:08 +0900

    [level 3] Title: 산 모양 타일링, Time: 3.67 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] tops) {
+        int[] d = new int[100005];
+        int[] p = new int[100005];
+        d[0] = 1; p[0] = 1;
+        d[1] = tops[0] == 1 ? 4 : 3;
+        p[1] = tops[0] == 1 ? 3 : 2;
+        for(int i = 1; i <= n; i++) {
+            if(tops[i - 1] == 1) {
+                p[i] = (d[i - 1] * 2 + p[i - 1]) % 10007;
+                d[i] = (p[i] + d[i - 1]) % 10007;
+            } else {
+                p[i] = (d[i - 1] + p[i - 1]) % 10007;
+                d[i] = (p[i] + d[i - 1]) % 10007;
+            }
+        }
+        return d[n];
+    }
+}
