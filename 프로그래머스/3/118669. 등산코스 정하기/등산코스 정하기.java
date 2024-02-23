39972f43730c9cc33a6498e6c22480f62d4f4fc1
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-02-23 19:51:23 +0900

    [level 3] Title: 등산코스 정하기, Time: 119.15 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,6 +1,66 @@
+import java.util.*;
+
 class Solution {
+    static ArrayList<Edge>[] edges;
+    static HashSet<Integer> hs = new HashSet<>();
+    static class Edge implements Comparable<Edge> {
+        int w, v;
+        public Edge(int w, int v) {
+            this.w = w;
+            this.v = v;
+        }
+        @Override
+        public int compareTo(Edge e) {
+            return this.w - e.w;
+        }
+    }
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] answer = {};
-        return answer;
+        for(int i = 0; i < gates.length; i++) hs.add(gates[i]);
+        for(int i = 0; i < summits.length; i++) hs.add(summits[i]);
+        
+        edges = new ArrayList[n + 1];
+        for(int i = 1; i <= n; i++) {
+            edges[i] = new ArrayList<>();
+        }
+        int[] d = new int[n + 1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        
+        for(int i = 0; i < paths.length; i++) {
+            int from = paths[i][0];
+            int to = paths[i][1]; 
+            int weight = paths[i][2];
+            edges[from].add(new Edge(weight, to));
+            edges[to].add(new Edge(weight, from));
+        }
+        
+        Queue<Edge> pq = new PriorityQueue<>();
+        for(int i = 0; i < gates.length; i++) {
+            d[gates[i]] = 0;
+            pq.offer(new Edge(d[gates[i]], gates[i]));
+        }
+        
+        while(!pq.isEmpty()) {
+            Edge cur = pq.poll();
+            if(d[cur.v] != cur.w) continue;
+            for(Edge nxt : edges[cur.v]) {
+                if(d[nxt.v] <= Math.max(d[cur.v], nxt.w)) continue;
+                d[nxt.v] = Math.max(d[cur.v], nxt.w);
+                if(hs.contains(nxt.v)) continue;
+                pq.offer(new Edge(d[nxt.v], nxt.v));
+            }
+        }
+        
+        int intensity = Integer.MAX_VALUE;
+        int summit = Integer.MAX_VALUE;
+        for(int i = 0; i < summits.length; i++) {
+            if(d[summits[i]] == intensity) {
+                summit = Math.min(summit, summits[i]);
+            }
+            if(d[summits[i]] < intensity) {
+                intensity = d[summits[i]];
+                summit = summits[i];
+            }
+        }
+        return new int[] {summit, intensity};
     }
 }
