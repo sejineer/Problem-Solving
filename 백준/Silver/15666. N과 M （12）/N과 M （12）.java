9f9c5078e7e9033b75f878462443ad52e9c052cd
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-03-14 03:07:53 +0900

    [Silver II] Title: N과 M (12), Time: 176 ms, Memory: 15736 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] nums = new int[10];
+    static int[] arr = new int[10];
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static void func(int k) throws IOException {
+        if(k == m) {
+            for(int i = 0; i < m; i++) {
+                bw.write(nums[arr[i]] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        int tmp = 0;
+        int st = 0;
+        if(k != 0) st = arr[k - 1];
+        for(int i = st; i < n; i++) {
+            if(tmp != nums[i]){
+                arr[k] = i;
+                tmp = nums[i];
+                func(k + 1);  
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums, 0, n);
+        func(0);
+        bw.flush();
+    }
+}
