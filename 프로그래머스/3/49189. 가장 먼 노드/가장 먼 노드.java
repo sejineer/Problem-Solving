89f0a3b188cab1ad095e37a6c1474b594fa1057f
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-06-12 13:09:50 +0900

    [level 3] Title: 가장 먼 노드, Time: 81.68 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] edge) {
+        List<List<Integer>> graph = new ArrayList<>(n + 1);
+        for(int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for(int[] i : edge) {
+            graph.get(i[0]).add(i[1]);
+            graph.get(i[1]).add(i[0]);
+        }
+        
+        int[] dist = new int[n + 1];
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        dist[1] = 1;
+        
+        int max = 0;
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+            for(int i : graph.get(cur)) {
+                if(dist[i] > 0) continue;
+                dist[i] = dist[cur] + 1;
+                max = Math.max(max, dist[i]);
+                queue.add(i);
+            }
+        }
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            System.out.print(dist[i] + " ");
+            if(dist[i] == max) count++;
+        }
+        return count;
+    }
+}
