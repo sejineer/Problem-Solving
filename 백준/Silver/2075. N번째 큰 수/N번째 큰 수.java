a9982eec0834b7e9db85c060224bcdb20d32125f
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-06-26 06:38:12 +0900

    [Silver II] Title: N번째 큰 수, Time: 880 ms, Memory: 282236 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int[][] board;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        board = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int j = 0; j < n; j++) {
+            for(int i = n - 1; i >= 0; i--) {
+                pq.add(board[i][j]);
+            }
+        }
+
+        for(int i = 0; i < n - 1; i++) {
+            pq.poll();
+        }
+
+        System.out.println(pq.poll());
+    }
+}
