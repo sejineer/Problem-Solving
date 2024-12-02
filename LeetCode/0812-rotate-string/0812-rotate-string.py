a83b28014a81a50609bb758278856ab5b95dff9d
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-12-02 17:29:58 +0900

    Time: 0 ms (100%), Space: 17.3 MB (19.4%) - LeetHub

--- LeetCode/0812-rotate-string/0812-rotate-string.py
+++ LeetCode/0812-rotate-string/0812-rotate-string.py
@@ -3,7 +3,6 @@ from collections import deque
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         deq = deque(s)
-        print(deq)
         for _ in range(len(s)):
             first = deq.popleft()
             deq.append(first)
