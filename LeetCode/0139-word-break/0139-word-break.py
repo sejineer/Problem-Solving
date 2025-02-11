171167703a6706959a958f4dff92586519658c09
Sejin Park <95167215+sejineer@users.noreply.github.com>
2025-02-11 17:21:34 +0900

    Time: 0 ms (100%), Space: 17.9 MB (60.38%) - LeetHub

---
+++ LeetCode/0139-word-break/0139-word-break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        @cache
+        def dfs(start):
+            if start == len(s):
+                return True
+
+            for word in wordDict:
+                if s[start : start + len(word)] == word:
+                    if dfs(start + len(word)):
+                        return True
+            return False
+        
+        return dfs(0)
+            
