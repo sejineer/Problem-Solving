59baa03a01474fc5a57675631415f98d680534ca
Sejin Park <95167215+sejineer@users.noreply.github.com>
2025-04-05 13:56:48 +0900

    Time: 3 ms (7.87%), Space: 17.8 MB (54.48%) - LeetHub

---
+++ LeetCode/0198-house-robber/0198-house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        def backtracking(k: int) -> int:
+            if k >= len(nums):
+                return 0
+            if k in memo:
+                return memo[k]
+            
+            rob = nums[k] + backtracking(k + 2)
+            skip = backtracking(k + 1)
+
+            memo[k] = max(rob, skip)
+            return memo[k]
+        
+        return backtracking(0)
