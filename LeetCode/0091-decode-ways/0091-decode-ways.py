9f1141791f70792f13948628dae8c7256efd9da8
Sejin Park <95167215+sejineer@users.noreply.github.com>
2025-04-15 23:38:42 +0900

    Time: 0 ms (100%), Space: 18 MB (30.33%) - LeetHub

--- LeetCode/0091-decode-ways/0091-decode-ways.py
+++ LeetCode/0091-decode-ways/0091-decode-ways.py
@@ -1,12 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        cur, nxt = 1, 0
-        for start in reversed(range(len(s))):
-            if s[start] == "0":
-                cur, nxt = 0, cur
-            elif start + 1 < len(s) and int(s[start : start + 2]) < 27:
-                cur, nxt = cur + nxt, cur
+        dp = [0] * len(s) + [1]
+
+        for i in reversed(range(len(s))):
+            if s[i] == "0":
+                dp[i] = 0
+            elif i + 1 < len(s) and int(s[i : i + 2]) < 27:
+                dp[i] = dp[i + 1] + dp[i + 2]
             else:
-                nxt = cur
-        return cur
+                dp[i] = dp[i + 1]
         
+        return dp[0]
+                
