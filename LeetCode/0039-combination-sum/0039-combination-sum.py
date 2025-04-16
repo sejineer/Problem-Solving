1064de0ad301544d3a7e4c8b5cf6a055293279b9
Sejin Park <95167215+sejineer@users.noreply.github.com>
2025-04-17 00:47:11 +0900

    Time: 3 ms (97.43%), Space: 18.1 MB (5.08%) - LeetHub

--- LeetCode/0039-combination-sum/0039-combination-sum.py
+++ LeetCode/0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,9 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
-        def backtracking(start: int, s: int, num_list: List[int]):
-            if s > target:
-                return
-            elif s == target:
-                result.append(num_list[:])
-                return
-            
-            for idx in range(start, len(candidates)):
-                num = candidates[idx]
-                num_list.append(num)
-                backtracking(idx, s + num, num_list)
-                num_list.pop()
-        
-        backtracking(0, 0, [])
-        return result
-            
+        dp = [[] for _ in range(target + 1)]
+        dp[0] = [[]]
+        for candidate in candidates:
+            for num in range(candidate, target + 1):
+                for combination in dp[num - candidate]:
+                    dp[num].append(combination + [candidate])
+        return dp[target]
