d3a5a719f40b13e5f16806552588005db51bd33c
Sejin Park <95167215+sejineer@users.noreply.github.com>
2025-05-01 10:17:53 +0900

    Time: 47 ms (5.89%), Space: 31.2 MB (5.52%) - LeetHub

--- LeetCode/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,10 @@ from collections import Counter, defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
-        for word in strs:
-            anagrams[frozenset(Counter(word).items())].append(word)
-        return list(anagrams.values())
+        strs_dict = defaultdict(list)
+        for s in strs:
+            s_counter = Counter(s)
+            strs_dict[frozenset(s_counter.items())].append(s)
+        
+        return list(strs_dict.values())
+            
