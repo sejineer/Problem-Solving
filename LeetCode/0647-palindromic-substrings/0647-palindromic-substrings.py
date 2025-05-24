599193f7fa3ee3a13a24d3143b88bb51bf0ac6fa
Sejin Park <95167215+sejineer@users.noreply.github.com>
2025-05-24 10:33:25 +0900

    Time: 127 ms (77.33%), Space: 17.8 MB (66.26%) - LeetHub

--- LeetCode/0647-palindromic-substrings/0647-palindromic-substrings.py
+++ LeetCode/0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,7 +1,7 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = 0
-        def count_palindrom(left: int, right: int):
+        result = 0
+        def count_palindrome(left: int, right: int):
             count = 0
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 count += 1
@@ -10,7 +10,6 @@ class Solution:
             return count
         
         for i in range(len(s)):
-            res += count_palindrom(i, i)
-            res += count_palindrom(i, i + 1)
-        return res
-                    
+            result += count_palindrome(i, i)
+            result += count_palindrome(i, i + 1)
+        return result
